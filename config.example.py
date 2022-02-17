c40caea3d058f0c88f7ece723704372a2542a5bc
Michael Bulebush <michael.bulebush@gmail.com>
2022-02-17 11:40:43 -0500

    Reorganized to indicate initial setup and streamline.

---
+++ config.example.py
@@ -0,0 +1,20 @@
+#General settings
+
+#Database Settings
+dbname = 'database name'  # add your database name
+dbuser = 'db account' # add your username
+dbpass = 'db password' # add your password
+dbhost = 'localhost'
+dbport = '5432'
+dbsslmode = 'require'
+
+#Alpaca specific settings
+alp_apikey = 'your alpaca key DHHK'
+alp_secret = 'Your Alpaca Secret 9GotvKTSqao3Il7hgeqbF'
+alp_base_url = 'https://api.alpaca.markets'
+
+#TD Ameritrade specific settings
+#tda_apikey = '6VFR2GQRK7K9RG85OPD5SJHD45DQAXOL@AMER.OAUTHAP'  Didn't need this
+tda_apikey = 'TD Ameritrade api from my apps in dev site'
+tda_token_path = 'token'
+tda_redirect_uri = 'http://localhost'
